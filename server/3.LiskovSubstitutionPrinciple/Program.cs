1a9513ca7ee2cb0c257be8d3fef86531988dae4b
amolw-cuelogic <amol.wabale@cuelogic.com>
2018-03-22 17:44:00 +0530

    Initial commit

---
+++ server/3.LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,27 @@
+﻿using _3.LiskovSubstitutionPrinciple.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3.LiskovSubstitutionPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Export obj;
+            if (1 == 1)//PDF
+            {
+                obj = new ExportToPdf(); // Export to pdf can be substituted
+            }
+            else
+            {
+                obj = new ExportToExcel();
+            }
+
+                obj.Convert(@"c:\demo.txt");
+            
+        }
+    }
+}
